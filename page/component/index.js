425caea41c922b9f4d4a183a43a9626a97e5fdf9
Yu Abigale <yupeichun@kingsoft.com>
2017-07-02 15:20:25 +0800

    reduce video column

--- page/component/index.js
+++ page/component/index.js
@@ -1,51 +1,9 @@
 Page({
     data: {
-        list: [{
-                id: 'demo',
-                name: 'Demo',
-                pages: ['demo']
-            }, {
+        list: [ {
                 id: 'abigale',
                 name: 'Abigale',
                 pages: ['friends']
-            }, {
-                id: 'temp',
-                name: 'temp',
-                pages: ['temp-scroll-view']
-            },
-            {
-                id: 'view',
-                name: '视图容器',
-                open: false,
-                pages: ['view', 'scroll-view', 'swiper']
-            }, {
-                id: 'content',
-                name: '基础内容',
-                open: false,
-                pages: ['text', 'icon', 'progress']
-            }, {
-                id: 'form',
-                name: '表单组件',
-                open: false,
-                pages: ['button', 'checkbox', 'form', 'input', 'label', 'picker', 'radio', 'slider', 'switch', 'textarea']
-            }, {
-                id: 'nav',
-                name: '导航',
-                open: false,
-                pages: ['navigator']
-            }, {
-                id: 'media',
-                name: '媒体组件',
-                open: false,
-                pages: ['image', 'audio', 'video']
-            }, {
-                id: 'map',
-                name: '地图',
-                pages: ['map']
-            }, {
-                id: 'canvas',
-                name: '画布',
-                pages: ['canvas']
             }
         ]
     },
